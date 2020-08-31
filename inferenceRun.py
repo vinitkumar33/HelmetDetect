b5dfa17f2471d78ddc09375bf002eb66eff61afd
I351150 <vinit.kumar01@sap.com>
2020-08-31 19:08:30 +0530

    First commit.  Please enter the commit message for your changes. Lines starting

---
+++ inferenceRun.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+import numpy as np
+import cv2 as cv
+from PIL import Image
+from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
+from object_detection.utils import ops as utils_ops
+
+class DefectDetection(object):
+ 
+    def show_inference(self, model, image_path):
+      # the array based representation of the image will be used later in order to prepare the
+      # result image with boxes and labels on it.
+      image_np = np.array(Image.open(image_path))
+      PATH_TO_LABELS = 'helmets_label_map.pbtxt'
+      # Actual detection.
+      output_dict = self.run_inference_for_single_image(model, image_np)
+      label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+      categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=2, use_display_name=True)
+      category_index = label_map_util.create_category_index(categories)
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          output_dict['detection_boxes'],
+          output_dict['detection_classes'],
+          output_dict['detection_scores'],
+          category_index,
+          instance_masks=output_dict.get('detection_masks_reframed', None),
+          use_normalized_coordinates=True,
+          line_thickness=8)
+
+      #display(Image.fromarray(image_np))
+      cv.imshow('window',image_np)
+      if cv.waitKey(95000):# & 0xFF == ord('q'):
+          cv.destroyAllWindows()
+
+    def run_inference_for_single_image(self, model, image):
+      image = np.asarray(image)
+      # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+      input_tensor = tf.convert_to_tensor(image)
+      # The model expects a batch of images, so add an axis with `tf.newaxis`.
+      input_tensor = input_tensor[tf.newaxis,...]
+
+      # Run inference
+      output_dict = model(input_tensor)
+
+      # All outputs are batches tensors.
+      # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+      # We're only interested in the first num_detections.
+      num_detections = int(output_dict.pop('num_detections'))
+      output_dict = {key:value[0, :num_detections].numpy() 
+                     for key,value in output_dict.items()}
+      output_dict['num_detections'] = num_detections
+
+      # detection_classes should be ints.
+      output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+       
+      # Handle models with masks:
+      if 'detection_masks' in output_dict:
+        # Reframe the the bbox mask to the image size.
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                  output_dict['detection_masks'], output_dict['detection_boxes'],
+                   image.shape[0], image.shape[1])      
+        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
+                                           tf.uint8)
+        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+        
+      return output_dict
+
+def load_model(model_name):
+    model_dir = "model"
+
+    #localhost_save_option = tf.saved_model.SaveOptions(experimental_io_device="/job:localhost")
+    #model.save(model_dir, options=localhost_save_option)
+
+    # Restore the weights
+    #model = tf.keras.models.load_model(model_dir, options=localhost_save_option)
+    #model_dir = '/Users/i351150/Downloads/model'
+
+    localhost_load_option=tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
+    #localhost_save_option = tf.saved_model.SaveOptions(experimental_io_device='/job:localhost')
+    model = tf.saved_model.load(model_dir, tags=None, options=localhost_load_option)
+
+    #model = tf.keras.models.load_model(model_dir, custom_objects=None, compile=True, options=None)
+    #model.save(model_dir, options=localhost_save_option)
+    #model = model.signatures['serving_default']
+
+    return model
+
+if __name__ == "__main__":
+    classifier = DefectDetection()
+    model_name = 'frozen_inference_graph.pb'
+    detection_model = load_model(model_name)
+    classifier.show_inference(detection_model, "testimage/image3.jpg")
---
+++ inference_v2 copy.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+import cv2 as cv
+
+class DefectDetection:
+
+    def __init__(self):
+
+        # Path to frozen detection graph. This is the actual model that is used for the object detection.
+        PATH_TO_CKPT = 'saved_model.pb'
+
+        self.detection_graph = tf.Graph()
+        self.confidence = .50
+
+        with self.detection_graph.as_default():
+            od_graph_def = tf.compat.v1.GraphDef()
+            with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+    def detect(self, img_np, img_box):
+
+        detection_graph = self.detection_graph
+        # labels = []
+        label = None
+        with detection_graph.as_default():
+            with tf.compat.v1.Session(graph=detection_graph) as sess:
+
+                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                img_expanded = np.expand_dims(img_np, axis=0)
+
+                # Actual detection.
+                (boxes, scores, classes, num) = sess.run(
+                    [detection_boxes, detection_scores, detection_classes, num_detections],
+                    feed_dict={image_tensor: img_expanded})
+
+                box = tuple(boxes[0].tolist())
+                for score_seq, score in enumerate(np.nditer(scores[0])):
+                    if score < self.confidence:
+                        break
+                    print(score)
+                    # labels.append(int(classes[0][score_seq]))
+                    label = int(classes[0][score_seq])
+                    ymin, xmin, ymax, xmax = tuple(box[score_seq])
+                    im_height, im_width = img_box.shape[:2]
+                    (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                                  ymin * im_height, ymax * im_height)
+                    cv.rectangle(img_box, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 4)
+
+        return (label, img_box)
+
+if __name__ == "__main__":
+    classifier = DefectDetection()
+    img_box = cv.imread('image1.jpeg')
+    img_np = cv.cvtColor(img_box, cv.COLOR_RGB2BGR)
+    labels, img_box = classifier.detect(img_np, img_box)
+    cv.imwrite("image.jpg", img_box)
